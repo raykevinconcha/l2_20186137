d1950b43b160b7cd26f6cca8f508e3a2cfc1c415
raykevinconcha <a20186137@pucp.edu.pe>
2023-09-15 23:11:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/l2_20186137/sing_up.java
@@ -0,0 +1,14 @@
+package com.example.l2_20186137;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sing_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
