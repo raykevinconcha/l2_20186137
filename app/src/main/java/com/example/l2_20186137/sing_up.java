79066ec681c5a4506b6b1498a1390ae5ecb0d663
raykevinconcha <a20186137@pucp.edu.pe>
2023-09-16 18:13:47 -0500

    crear cuenta

--- app/src/main/java/com/example/l2_20186137/sing_up.java
+++ app/src/main/java/com/example/l2_20186137/sing_up.java
@@ -2,13 +2,32 @@ package com.example.l2_20186137;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class sing_up extends AppCompatActivity {
-
+    private CheckBox cbAceptar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+        cbAceptar=findViewById(R.id.cbAceptar);
+        Button btncreateaccount= findViewById(R.id.buttoncreate);
+        btncreateaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cbAceptar.isChecked()){
+                    Intent i = new Intent(sing_up.this, Username.class);
+                    startActivity(i);
+
+                }else{
+                    Toast.makeText(sing_up.this,"Verifique la casilla porfavor para crear su cuenta",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
