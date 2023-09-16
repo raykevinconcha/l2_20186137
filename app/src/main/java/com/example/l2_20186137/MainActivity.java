0c7034eda1600e421f2093eaa8915f3283b57a0b
raykevinconcha <a20186137@pucp.edu.pe>
2023-09-16 17:22:13 -0500

    google

--- app/src/main/java/com/example/l2_20186137/MainActivity.java
+++ app/src/main/java/com/example/l2_20186137/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.l2_20186137;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup=(Button) findViewById(R.id.button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, sing_up.class);
+                startActivity(i);
+            }
+        });
     }
 }
