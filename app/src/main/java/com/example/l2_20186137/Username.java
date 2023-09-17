2d9706d1c2152d90a923a7c3b2e439474a2cbdb3
raykevinconcha <a20186137@pucp.edu.pe>
2023-09-16 19:18:35 -0500

    contador y cronometro (falta)

--- app/src/main/java/com/example/l2_20186137/Username.java
+++ app/src/main/java/com/example/l2_20186137/Username.java
@@ -2,13 +2,35 @@ package com.example.l2_20186137;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Username extends AppCompatActivity {
-
+    Button cronometro;
+    Button contador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_username);
+        cronometro = (Button) findViewById(R.id.button3);
+        contador = (Button) findViewById(R.id.button4);
+        cronometro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Username.this, cronometro.class);
+                startActivity(i);
+
+            }
+        });
+        contador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j = new Intent(Username.this, Contador.class);
+                startActivity(j);
+            }
+        });
     }
-}
+
+    }
